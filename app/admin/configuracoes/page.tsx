21ed1f631fe3292640882d526ab8987188952bc2
kvvv1 <kaikevns1@gmail.com>
2025-08-15 09:45:29 -0300

    Make /admin/configuracoes page dynamic to fix prerender error

--- app/admin/configuracoes/page.tsx
+++ app/admin/configuracoes/page.tsx
@@ -1,6 +1,8 @@
 import { AdminLayout } from "@/components/admin/admin-layout"
 import { AdminSettings } from "@/components/admin/admin-settings"
 
+export const dynamic = "force-dynamic"
+
 export default function AdminSettingsPage() {
   return (
     <AdminLayout>
